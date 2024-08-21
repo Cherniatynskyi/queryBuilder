6764fcb8ba8a77f8db3709f4359da3f6f75dce84
Mark Cherniatynskyi <markushalot34@gmail.com>
2024-08-21 14:43:13 +0300

    backup

---
+++ src/app_backup/page.tsx
@@ -0,0 +1,9 @@
+import HomePage from "@/pages";
+
+export default function Home() {
+    return (
+      <main className="flex bg-white min-h-screen flex-col items-center justify-between p-24">
+        <HomePage></HomePage>
+      </main>
+    );
+  }
